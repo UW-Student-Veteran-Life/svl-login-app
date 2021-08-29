1e1840730da76b74b1e5438c49602cf0c15fe309
Harmeet Singh <harm2305@uw.edu>
2021-08-29 16:06:31 -0700

    Logging user now checks for valid attributes and returns logged data

--- routes.js
+++ routes.js
@@ -4,9 +4,29 @@ const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
+let containsAttr = (obj, attrs) => {
+  for(const attr of attrs) {
+    if(!Object.keys(obj).includes(attr)) {
+      return false;
+    }
+    return true;
+  }
+}
+
 router.post('/logUser', (req, res) => {
-  console.log(req.body)
-  res.send('I am working!');
+  let userData = req.body;
+  let requiredAttrs = ['name', 'netid', 'sid', 'reason']
+
+  if (containsAttr(userData,  requiredAttrs)) {
+    let now  = new Date();
+    userData.timestamp = now.toISOString();
+    userData.text = `User entry successfully recorded for ${userData.name} at ${now.toString()}`;
+    res.status(200).json(userData);
+  } else {
+    res.status(400).send({
+      'text': 'Please make sure you include all attributes for this request'
+    });
+  }
 });
 
 module.exports = router;
