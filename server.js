58f0f53163a088cce0535c0397738b17d9bd8451
Harmeet Singh <harmeets0825@gmail.com>
2021-11-06 17:41:08 -0700

    Added certs to server

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const app = express();
-const port = 5000 | process.env.port
+const port = 5000 | process.env.port;
 
 let routes = require('./routes');
-
 app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+httpsOptions = {
+  pfx: fs.readFileSync(`/var/ssl/private/${process.env.WEBSITE_LOAD_CERTIFICATES}`),
+  passphrase: process.env.CERT_PASS
+}
+
+https.createServer(httpsOptions, app).listen(port, () => {
+  console.log(`Server started on port: ` + port);
 });
