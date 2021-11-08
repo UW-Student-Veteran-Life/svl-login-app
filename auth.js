037fb0132cfd15eefdd499f07bf223fd7aca95ee
Harmeet Singh <harmeets0825@gmail.com>
2021-11-07 19:47:41 -0800

    Initial commit

---
+++ auth.js
@@ -0,0 +1,19 @@
+const { ClientSecretCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
+
+async function getCert() {
+  // Authentication for Azure Active Directory
+  const credential = new ClientSecretCredential(
+    process.env.AZURE_TENANT_ID,
+    process.env.AZURE_CLIENT_ID,
+    process.env.AZURE_CLIENT_SECRET
+  );
+  const vaultUrl = 'https://svl-keyvault.vault.azure.net/';
+
+  const client = new SecretClient(vaultUrl, credential)
+  const cert = await client.getSecret('svlcardreader-vetlife-washington-edu');
+
+  return cert;
+}
+
+module.exports = getCert;
