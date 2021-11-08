0792344dd2a210a835ddfd17a69a34971cedbd90
Harmeet Singh <harmeets0825@gmail.com>
2021-11-08 14:58:26 -0800

    Test server

--- server.js
+++ server.js
@@ -4,24 +4,31 @@ const getCert = require('./auth');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Get certificate from KeyVault and then start server
-getCert()
-.then(cert => {
-  httpsOptions = {
-    pfx: new Buffer.from(cert.value, 'base64')
-  }
+// // Get certificate from KeyVault and then start server
+// getCert()
+// .then(cert => {
+//   httpsOptions = {
+//     pfx: new Buffer.from(cert.value, 'base64')
+//   }
 
-  requestOptions = {
-    hostname: `wseval.s.uw.edu`,
-    method: 'GET',
-    pfx: new Buffer.from(cert.value, 'base64')
-  }
+//   requestOptions = {
+//     hostname: `wseval.s.uw.edu`,
+//     method: 'GET',
+//     pfx: new Buffer.from(cert.value, 'base64')
+//   }
 
-  // Router middleware setup
-  let initRoutes = require('./routes');
-  app.use('/', initRoutes(requestOptions));
+//   // Router middleware setup
+//
+//   app.use('/', initRoutes(requestOptions));
 
-  https.createServer(httpsOptions, app).listen(port, () => {
-    console.log(`Server started on port: ` + port);
-  });
-}).catch(err => console.error(err));
+//   https.createServer(httpsOptions, app).listen(port, () => {
+//     console.log(`Server started on port: ` + port);
+//   });
+// }).catch(err => console.error(err));
+
+let initRoutes = require('./routes');
+app.use('/', initRoutes());
+
+https.createServer({}, app).listen(port, () => {
+  console.log(`Server started on port: ` + port);
+});
