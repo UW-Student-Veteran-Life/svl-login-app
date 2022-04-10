59930ef2bfd0a611e54f316bed290636fba9ae12
Harmeet <harmeets0825@gmail.com>
2022-04-10 11:43:03 -0700

    Updated query to use start and end timestamps

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 const { CosmosClient } = require('@azure/cosmos');
 const dbClient = new CosmosClient(process.env.cosmos_db_conn);
 const database = dbClient.database('SVL');
-const container = database.container('Logins');
+const container = database.container(process.env.database);
 
 async function logEntry(data) {
   let item = await container.items.create(data);
   return item;
 }
 
-async function getItemsByDate(date) {
-  const { resources } = await container.items.query(`SELECT * FROM C WHERE C.date = "${date}"`).fetchAll();
+async function getItemsByDate(startDate, endDate = null) {
+  let qry = `SELECT * FROM C WHERE C.timestamp >= "${startDate}"`;
+
+  if (endDate) qry += `AND C.timestamp <= "${endDate}"`;
+
+  const { resources } = await container.items.query(qry).fetchAll();
   const results = [];
 
   for (const record of resources) {
