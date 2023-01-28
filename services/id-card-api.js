277a5aa3ffc8be9e4e4a91aaa472faebfc561ca3
Harmeet Singh <harm2305@uw.edu>
2023-01-27 19:12:36 -0800

    Added implementation to search using RFID

--- services/id-card-api.js
+++ services/id-card-api.js
@@ -15,7 +15,7 @@ const apiRoot = process.env.API_ROOT;
  * @param {string} magStripCode The mag strip code to get information for
  * @returns A Promise with the student's regId or an error
  */
-async function getStudentRegId(magStripCode) {
+async function getStudentRegIdByMag(magStripCode) {
   const certPfx = await kvSecretClient.getSecret('svlcardreader-vetlife-washington-edu');
 
   // Create an HTTPS agent with base64 encoded certificate
@@ -37,4 +37,31 @@ async function getStudentRegId(magStripCode) {
   }
 }
 
-module.exports = { getStudentRegId };
+/**
+ * Retrieves a student's reg id from the RFID associated with their Husky card
+ * @param {string} proxRfid RFID from Husky Card
+ * @returns {Promise<string>} The student's reg ID
+ */
+async function getStudentRegIdByProx(proxRfid) {
+  const certPfx = await kvSecretClient.getSecret('svlcardreader-vetlife-washington-edu');
+
+  // Create an HTTPS agent with base64 encoded certificate
+  const httpsAgent = new https.Agent({
+    pfx: new Buffer.from(certPfx.value, 'base64')
+  });
+
+  const requestUrl = new URL('/idcard/v1/card.json', apiRoot);
+  requestUrl.searchParams.append('prox_rfid', proxRfid);
+
+  try {
+    console.log(`Processing request ${requestUrl}`);
+    const response = await axios.get(requestUrl, { httpsAgent });
+    const studentData = response.data.Persons[0];
+    return studentData;
+  } catch (error) {
+    console.error(`There was an issue searching using prox_rfid ${proxRfid}: ${error.response.data.StatusDescription}`);
+    throw new Error(error.response.data.StatusDescription);
+  }
+}
+
+module.exports = { getStudentRegIdByMag, getStudentRegIdByProx };
