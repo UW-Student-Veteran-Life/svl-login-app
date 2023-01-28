65e4f9d00e2ccd219608d5d6699a1e317be060e7
Harmeet Singh <harm2305@uw.edu>
2023-01-27 22:28:38 -0800

    Ignored eslint warning

--- models/UserLogin.js
+++ models/UserLogin.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 const { Student } = require('./Student');
 
 /**
@@ -8,21 +9,21 @@ const { Student } = require('./Student');
  * @property {Date} createdAt JSON string of the date & time of login
  */
 class UserLogin {
-    student;
-    loginReason;
-    createdAt;
+  student;
+  loginReason;
+  createdAt;
 
-    /**
-     * 
-     * @param {Student} student Student associated with this login
-     * @param {string} loginReason Reason why the student is logging in
-     * @param {Date} createdAt Date the login was to created, set to the current system date if not specified
-     */
-    constructor(student, loginReason, createdAt = new Date()) {
-        this.student = student;
-        this.loginReason = loginReason;
-        this.createdAt = createdAt;
-    }
+  /**
+   * 
+   * @param {Student} student Student associated with this login
+   * @param {string} loginReason Reason why the student is logging in
+   * @param {Date} createdAt Date the login was to created, set to the current system date if not specified
+   */
+  constructor(student, loginReason, createdAt = new Date()) {
+    this.student = student;
+    this.loginReason = loginReason;
+    this.createdAt = createdAt;
+  }
 }
 
 module.exports = UserLogin;
