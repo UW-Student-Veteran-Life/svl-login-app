654d6ba25777d0964ada5c9f8ce5961650527c7a
Harmeet Singh <harm2305@uw.edu>
2023-01-28 00:20:44 -0800

    Refactored search functions into one function

--- services/id-card-api.js
+++ services/id-card-api.js
@@ -1,58 +1,34 @@
 const axios = require('axios');
 const https = require('https');
-const pfxCert = require('./cert');
+const { pfxCert } = require('./cert');
 const process = require('process');
 
 const apiRoot = process.env.API_ROOT;
 
 /**
- * Gets the student's RegId for a given a mag strip code
- * @param {string} magStripCode The mag strip code to get information for
- * @returns A Promise with the student's regId or an error
+ * Searches for the reg ID associated with a Husky card with the given identifier
+ * @param {string} cardIdentifier The identifier for the card
+ * @param {string} identifierType The identifier type, should be either mag_strip_code or prox_rfid
+ * @returns {Promise<string>} The reg ID for the user associated with the card
  */
-async function getStudentRegIdByMag(magStripCode) {
+async function searchCard(cardIdentifier, identifierType) {
   // Create an HTTPS agent with base64 encoded certificate
   const httpsAgent = new https.Agent({
     pfx: await pfxCert
   });
 
   const requestUrl = new URL('/idcard/v1/card.json', apiRoot);
-  requestUrl.searchParams.append('mag_strip_code', magStripCode);
+  requestUrl.searchParams.append(identifierType, cardIdentifier);
 
   try {
     console.log(`Processing request ${requestUrl}`);
     const response = await axios.get(requestUrl, { httpsAgent });
-    const studentData = response.data.Persons[0];
-    return studentData;
+    const regId = response.data.Cards[0].RegID;
+    return regId;
   } catch (error) {
-    console.error(`There was an issue searching using mag_strip_code ${magStripCode}: ${error.response.data.StatusDescription}`);
-    throw new Error(error.response.data.StatusDescription);
+    console.error(`Unable to resolve ${identifierType} ${cardIdentifier}: ${error.message}`);
+    throw new Error(`Unable to resolve ${identifierType} ${cardIdentifier}, this card may no longer be active`);
   }
 }
 
-/**
- * Retrieves a student's reg id from the RFID associated with their Husky card
- * @param {string} proxRfid RFID from Husky Card
- * @returns {Promise<string>} The student's reg ID
- */
-async function getStudentRegIdByProx(proxRfid) {
-  // Create an HTTPS agent with base64 encoded certificate
-  const httpsAgent = new https.Agent({
-    pfx: await pfxCert
-  });
-
-  const requestUrl = new URL('/idcard/v1/card.json', apiRoot);
-  requestUrl.searchParams.append('prox_rfid', proxRfid);
-
-  try {
-    console.log(`Processing request ${requestUrl}`);
-    const response = await axios.get(requestUrl, { httpsAgent });
-    const studentData = response.data.Persons[0];
-    return studentData;
-  } catch (error) {
-    console.error(`There was an issue searching using prox_rfid ${proxRfid}: ${error.response.data.StatusDescription}`);
-    throw new Error(error.response.data.StatusDescription);
-  }
-}
-
-module.exports = { getStudentRegIdByMag, getStudentRegIdByProx };
+module.exports = { searchCard };
