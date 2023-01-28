beb3e0220d4187e017ad20b4eaa8a633b3f37d8b
Harmeet Singh <harm2305@uw.edu>
2023-01-27 22:01:35 -0800

    Refactored async startup

--- server.js
+++ server.js
@@ -10,21 +10,27 @@ const process = require('process');
 const server = express();
 const port = process.env.PORT || 8080;
 
+// Remove the x-powered-by header for security reasons
+server.set('x-powered-by', false);
+
 const loginsRouter = require('./routers/logins');
+
+const vaultUri = process.env.VAULT_URI;
+if (vaultUri == undefined) throw Error('The environment variable \'VAULT_URI\' cannot be undefined');
+
+const azureCred = new DefaultAzureCredential();
+const kvSecretClient = new SecretClient(vaultUri, azureCred);
+const cosmosClient = (async () => {
+  const dbConnSecret = await kvSecretClient.getSecret('db-conn');
+  const dbConn = dbConnSecret.value;
+  return new CosmosClient(dbConn);
+})();
 
 // Router middleware setup
 server.use(morgan('combined'));
 server.use(async (req, res, next) => {
   // Initalize connection to database
-  const vaultUri = process.env.VAULT_URI;
-  if (vaultUri == undefined) throw Error('The environment variable \'VAULT_URI\' cannot be undefined');
-  const azureCred = new DefaultAzureCredential();
-  const kvSecretClient = new SecretClient(vaultUri, azureCred);
-  const dbConn = await kvSecretClient.getSecret('db-conn');
-  const dbClient = new CosmosClient(dbConn.value);
-  const database = dbClient.database('SVL');
-
-  req.database = database;
+  req.database = (await cosmosClient).database('SVL');
   next();
 });
 
